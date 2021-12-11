4515a566b21815b8c714276ffa07783eb149f897
falcon <falconhawk@protonmail.com>
2021-12-11 19:38:21 +0900

    add websocket code

---
+++ pkg/realtime_update/client.go
@@ -0,0 +1,71 @@
+package realtime_update
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait  = 10 * time.Second
+	pongWait   = 60 * time.Second
+	pingPeriod = (pongWait * 9) / 10
+)
+
+var newline = []byte{'\n'}
+
+type Client struct {
+	Conn *websocket.Conn
+	Send chan *Payload
+}
+
+func (c *Client) WritePump(h *Hub, channelName string) {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		h.Unregister <- &SubscriptionType{
+			ChannelName: channelName,
+			Client:      c,
+		}
+		c.Conn.Close()
+		fmt.Println("conn closeed")
+	}()
+	for {
+		select {
+		case message, ok := <-c.Send:
+			if err := c.Conn.SetWriteDeadline(time.Now().Add(writeWait)); err != nil {
+				return
+			}
+			if !ok {
+				// The hub closed the channel.
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.Conn.NextWriter(websocket.BinaryMessage)
+			if err != nil {
+				return
+			}
+
+			jsonResponse, err := json.Marshal(message)
+			if err != nil {
+				return
+			}
+
+			if _, err := w.Write(jsonResponse); err != nil {
+				return
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
---
+++ pkg/realtime_update/hub.go
@@ -0,0 +1,64 @@
+package realtime_update
+
+type Payload struct {
+	ChannelOne   float64 `json:"channelOne"`
+	ChannelTwo   float64 `json:"channelTwo"`
+	ChannelThree float64 `json:"channelThree"`
+	ChannelFour  float64 `json:"channelFour"`
+}
+
+type Message struct {
+	AccessKey string
+	Payload   *Payload
+}
+
+type SubscriptionType struct {
+	ChannelName string
+	Client      *Client
+}
+
+type Hub struct {
+	Subscriptions map[string]map[*Client]bool
+	Broadcast     chan *Message
+	Register      chan *SubscriptionType
+	Unregister    chan *SubscriptionType
+}
+
+func New() *Hub {
+	return &Hub{
+		Broadcast:     make(chan *Message),
+		Register:      make(chan *SubscriptionType),
+		Unregister:    make(chan *SubscriptionType),
+		Subscriptions: make(map[string]map[*Client]bool),
+	}
+}
+
+func (h *Hub) Listen() {
+	for {
+		select {
+		case subscription := <-h.Register:
+			channelSubscription, ok := h.Subscriptions[subscription.ChannelName]
+			if !ok {
+				h.Subscriptions[subscription.ChannelName] = make(map[*Client]bool)
+				channelSubscription = h.Subscriptions[subscription.ChannelName]
+			}
+			channelSubscription[subscription.Client] = true
+		case subscription := <-h.Unregister:
+			if _, ok := h.Subscriptions[subscription.ChannelName]; ok {
+				if _, ok := h.Subscriptions[subscription.ChannelName][subscription.Client]; ok {
+					delete(h.Subscriptions[subscription.ChannelName], subscription.Client)
+					close(subscription.Client.Send)
+				}
+			}
+		case message := <-h.Broadcast:
+			for client := range h.Subscriptions[message.AccessKey] {
+				select {
+				case client.Send <- message.Payload:
+				default:
+					close(client.Send)
+					delete(h.Subscriptions[message.AccessKey], client)
+				}
+			}
+		}
+	}
+}
